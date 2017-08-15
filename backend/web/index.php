ee172b37f30b3d476151e5c787822c8b26b880f3
neng liu <nengliu@nengdeMacBook-Pro.local>
2017-08-15 11:54:08 +0700

    up

---
+++ backend/web/index.php
@@ -0,0 +1,21 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+define ('APP_ROOT', dirname(__FILE__) .DIRECTORY_SEPARATOR. '..'.DIRECTORY_SEPARATOR.'vendor' );
+require_once(APP_ROOT . '/Bootstrap/Autoloader.php');
+\Bootstrap\Autoloader::instance()->init();
+
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../../common/config/main-local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
+);
+
+(new yii\web\Application($config))->run();
