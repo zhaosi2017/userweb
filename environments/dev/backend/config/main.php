4b935f80c501c0dfab50015595f16b3da1badee0
neng liu <nengliu@nengdeMacBook-Pro.local>
2017-08-18 21:58:00 +0700

    up

---
+++ environments/dev/backend/config/main.php
@@ -0,0 +1,64 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'language'=>'zh-CN',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [
+        'redactor' => 'yii\redactor\RedactorModule',
+    ],
+    'defaultRoute'=>'default/index',
+    'components' => [
+        'request' => [
+//            'csrfParam' => '_csrf-backend',
+            'cookieValidationKey' => '0PZDdlzm_yBNXEaaw-YreetUMInaQnZG',
+        ],
+        'user' => [
+            'identityClass' => 'backend\models\Admin',
+            'enableAutoLogin' => true,
+            'loginUrl' => ['/login/index'],
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'ip2region' => [
+            'class' => '\xiaogouxo\ip2region\Geolocation',
+            'mode' => 'SEARCH_BTREE',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+
+    ],
+    'params' => $params,
+
+];
