ea72149ab072376bc24ba2b318201c3d061b56bf
zp4314 <zp4314@gmail.com>
2017-08-29 15:38:30 +0700

    udpate

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -44,7 +44,8 @@ class AuthController extends Controller
     public function beforeAction($action)
     {
 
-       file_put_contents('/tmp/userweb.log',var_export($this->getRequestContent(),true).PHP_EOL,8);
+       file_put_contents('/tmp/userweb.log',var_export(\Yii::$app->request->getHeaders(),true).PHP_EOL,8);
+        file_put_contents('/tmp/userweb.log',var_export(\Yii::$app->request->getRawBody(),true).PHP_EOL,8);
        return true;
     }
 
