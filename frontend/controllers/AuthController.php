e5738bc94b19e7c562636ad704640fe51780f93a
zp4314 <zp4314@gmail.com>
2017-08-29 16:13:41 +0700

    update

--- frontend/controllers/AuthController.php
+++ frontend/controllers/AuthController.php
@@ -44,9 +44,20 @@ class AuthController extends Controller
     public function beforeAction($action)
     {
 
-       file_put_contents('/tmp/userweb.log',var_export(\Yii::$app->request->getHeaders(),true).PHP_EOL,8);
-        file_put_contents('/tmp/userweb.log',var_export(\Yii::$app->request->getRawBody(),true).PHP_EOL,8);
-       return true;
+        if(defined('YII_ENV') && YII_ENV == 'dev') {
+            file_put_contents('/tmp/userweb.log', 'request---------'.PHP_EOL.var_export(\Yii::$app->request->getHeaders(), true) . PHP_EOL, 8);
+            file_put_contents('/tmp/userweb.log', var_export(\Yii::$app->request->getRawBody(), true) . PHP_EOL, 8);
+        }
+        return parent::beforeAction($action);
+    }
+
+
+    public function afterAction($action, $result)
+    {
+        if(defined('YII_ENV') && YII_ENV == 'dev') {
+            file_put_contents('/tmp/userweb.log', 'response ----------' .PHP_EOL. var_export($result, true) . PHP_EOL, 8);
+        }
+        return parent::afterAction($action, $result);
     }
 
     public function getRequestContent()
