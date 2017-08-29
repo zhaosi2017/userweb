aca3c0d569b8497fd43f32f1923ee2d7e9bca8a3
zhangqing <qingzhang931@gmail.com>
2017-08-29 11:53:23 +0700

    fix

--- common/services/ttsService/thirds/Nexmo.php
+++ common/services/ttsService/thirds/Nexmo.php
@@ -11,7 +11,13 @@ use common\services\ttsService\AbstruactThird;
 
 class Nexmo extends  AbstruactThird{
 
+    private $apiKey = '85704df7';
+    private $apiScret = '755026fdd40f34c2';
+    private $applicationId = '454eb4c4-1fdd-4b4b-9423-937c80f01bb8';
 
+    public function CallStart(){
+
+    }
 
 
 }
