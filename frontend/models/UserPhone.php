ab5ad19f88ed394db80716e96644e317698237b7
zp4314 <zp4314@gmail.com>
2017-09-01 16:26:42 +0700

    Update

--- frontend/models/UserPhone.php
+++ frontend/models/UserPhone.php
@@ -69,19 +69,34 @@ class UserPhone extends FActiveRecord
         if(!$this->validate('phone_country_code','user_phone_number'))
         {
             return $this->jsonResponse([],$this->getErrors(),'1',ErrCode::FAILURE);
-
         }
         $key = $this->phone_country_code.$this->user_phone_number;
         $redis = Yii::$app->redis;
         $_code = $redis->get($key);
         if($_code && $_code == $code)
         {
+            $userId = Yii::$app->user->id;
+            $user = UserPhone ::find()->where(['user_id'=>$userId , 'phone_country_code'=>$this->phone_country_code,'user_phone_number'=>$this->user_phone_number])->one();
+            if(!empty($user))
+            {
+                return $this->jsonResponse([],'该手机号已被添加，不能重复添加','1',ErrCode::COUNTRY_CODE_PHONE_EXIST);
+            }else{
+                $this->user_id = $userId;
+                $this->reg_time = time();
+                if($this->save())
+                {
+                    $redis->del($key);
+                    return $this->jsonResponse([],'操作成功','0',ErrCode::SUCCESS);
+                }else{
+                    return $this->jsonResponse([],'操作失败','1',ErrCode::DATA_SAVE_ERROR);
+                }
+            }
 
         }else{
             return $this->jsonResponse([],'验证码错误','1',ErrCode::CODE_ERROR);
         }
 
-        $userId = Yii::$app->user->id;
+
 
     }
 
