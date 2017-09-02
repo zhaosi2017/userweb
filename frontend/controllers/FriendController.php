89a44aeab4a75cb45d253f522c4f3968ef335d6b
zp4314 <zp4314@gmail.com>
2017-09-02 15:37:05 +0700

    Update

---
+++ frontend/controllers/FriendController.php
@@ -0,0 +1,59 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use frontend\model\WhiteLists\WhiteList;
+use frontend\models\Channel;
+use yii\db\Exception;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\helpers\Url;
+use frontend\models\User;
+use frontend\models\ErrCode;
+use frontend\models\UrgentContact;
+use frontend\models\Friends\Friends;
+use frontend\models\Friends\FriendsRequest;
+use frontend\models\Friends\FriendsGroup;
+
+class FriendController extends AuthController
+{
+
+    public $enableCsrfValidation = false;
+
+
+    public function behaviors()
+    {
+
+        $self = [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['search'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'search' => ['post'],
+
+                ],
+            ],
+        ];
+        $behaviors = parent::behaviors();
+        return array_merge($behaviors,$self);
+    }
+
+    /**
+     * 根据account 和nickname 搜索
+     */
+    public function actionSearch()
+    {
+
+    }
+
+
+}
