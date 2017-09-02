99c1390965c2b05d68c338129fbeec4240ae9bad
zhangqing <qingzhang931@gmail.com>
2017-09-02 11:48:11 +0700

    fix

--- common/services/appService/apps/callu.php
+++ common/services/appService/apps/callu.php
@@ -11,6 +11,7 @@ namespace  common\services\appService\apps;
 
 use common\services\ttsService\thirds\Sinch;
 use frontend\models\Channel;
+use frontend\models\Friends\Friends;
 use frontend\models\User;
 use common\services\ttsService\CallService;
 use frontend\models\CallRecord\CallRecord;
@@ -19,13 +20,13 @@ class callu {
     /**
      * @var User  用户
      */
-    public $user;
+    public $from_user;
 
     /**
      * @var 用户好友对象
      *
      */
-    public $friend;
+    public $to_user;
     /**
      * @var socket fd
      */
@@ -58,34 +59,37 @@ class callu {
         $data = json_decode($data );
         $user =  User::find()->where(['token'=>$data->token])->one();   //身份校验
         if(empty($user)){
-            $this->socket_server->push($this->socket_fd , 'token错误');
+            $this->sendText('token错误');
             return ;
         }
 
         $to_user = User::findOne(['account'=>$data->account]);
         if(empty($to_user)){
-            $this->socket_server->push($this->socket_fd , '不存在被叫的用户');
+            $this->sendText('不存在被叫的用户');
             return ;
         }
         if(!key_exists($data->call_type , CallRecord::$type_map)){
-            $this->socket_server->push($this->socket_fd , '呼叫类型错误');
+            $this->sendText('呼叫类型错误');
             return ;
         }
         $channel = Channel::findOne($data->channel_id);
         if(empty($channel)){
-            $this->socket_server->push($this->socket_fd, '呼叫类型错误');
+            $this->sendText('呼叫类型错误');
             return ;
         }
-        $this->user   = $user;
-        $this->friend = $to_user;
+        $this->from_user   = $user;
+        $this->to_user = $to_user;
 
-        $service =new  CallService(Sinch::class);
+        $service = new  CallService(Sinch::class);
         $service->from_user = $user;
         $service->to_user   = $to_user;
+
+        $friend = Friends::findOne(['user_id'=>$this->to_user->id , 'friend_id'=>$this->from_user->id]); //这里取的是主叫在被叫好友列表中的名字
+        $name = empty($friend->remark)?$this->user->nickname:$friend->remark;
         if($data->call_type == CallRecord::CALLRECORD_TYPE_UNURGENT){
-            $service->text  = $this->friend->nickname.' 呼叫您上线 '.$channel->name;
+            $service->text  = $name.' 呼叫您上线 '.$channel->name;
         }else{
-            $service->text  = '请转告 '.$this->friend->nickname.' 上线'.$channel->name;
+            $service->text  = '请转告 '.$this->to_user->nickname.' 上线'.$channel->name;
         }
         $service->app       = $this;
         $service->call_type = $data->call_type;
