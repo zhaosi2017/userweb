bb4e1300d306c75de38f637c6b83717b1fdacdb5
zp4314 <zp4314@gmail.com>
2017-09-25 10:58:00 +0700

    好友删除。只删除好友关系链

--- frontend/models/Friends/FriendsDelFrom.php
+++ frontend/models/Friends/FriendsDelFrom.php
@@ -41,10 +41,10 @@ class FriendsDelFrom extends Friends
             //要删除相关的信息
             $_friend = Friends::findOne(['user_id'=>$userId,'friend_id'=>$user->id]);
             $friend = Friends::findOne(['friend_id'=>$userId,'user_id'=>$user->id]);
-            $white = WhiteList::findOne(['uid'=>$userId,'white_uid'=>$user->id]);
-            $blacklist = BlackList::findOne(['uid'=>$userId,'black_uid'=>$user->id]);
-            $_white = WhiteList::findOne(['uid'=>$user->id,'white_uid'=>$userId]);
-            $_blacklist = BlackList::findOne(['uid'=>$user->id,'black_uid'=>$userId]);
+//            $white = WhiteList::findOne(['uid'=>$userId,'white_uid'=>$user->id]);
+//            $blacklist = BlackList::findOne(['uid'=>$userId,'black_uid'=>$user->id]);
+//            $_white = WhiteList::findOne(['uid'=>$user->id,'white_uid'=>$userId]);
+//            $_blacklist = BlackList::findOne(['uid'=>$user->id,'black_uid'=>$userId]);
 
 
             if(empty($_friend))
@@ -66,38 +66,38 @@ class FriendsDelFrom extends Friends
                         return $this->jsonResponse([],$friend->getErrors(),'1',ErrCode::FAILURE);
                     }
                 }
-                if(!empty($white))
-                {
-                    if(!$white->delete())
-                    {
-                        $transaction->rollBack();
-                        return $this->jsonResponse([],$white->getErrors(),'1',ErrCode::FAILURE);
-                    }
-                }
-                if(!empty($blacklist))
-                {
-                    if(!$blacklist->delete())
-                    {
-                        $transaction->rollBack();
-                        return $this->jsonResponse([],$blacklist->getErrors(),'1',ErrCode::FAILURE);
-                    }
-                }
-                if(!empty($_white))
-                {
-                    if(!$_white->delete())
-                    {
-                        $transaction->rollBack();
-                        return $this->jsonResponse([],$_white->getErrors(),'1',ErrCode::FAILURE);
-                    }
-                }
-                if(!empty($_blacklist))
-                {
-                    if(!$_blacklist->delete())
-                    {
-                        $transaction->rollBack();
-                        return $this->jsonResponse([],$_blacklist->getErrors(),'1',ErrCode::FAILURE);
-                    }
-                }
+//                if(!empty($white))
+//                {
+//                    if(!$white->delete())
+//                    {
+//                        $transaction->rollBack();
+//                        return $this->jsonResponse([],$white->getErrors(),'1',ErrCode::FAILURE);
+//                    }
+//                }
+//                if(!empty($blacklist))
+//                {
+//                    if(!$blacklist->delete())
+//                    {
+//                        $transaction->rollBack();
+//                        return $this->jsonResponse([],$blacklist->getErrors(),'1',ErrCode::FAILURE);
+//                    }
+//                }
+//                if(!empty($_white))
+//                {
+//                    if(!$_white->delete())
+//                    {
+//                        $transaction->rollBack();
+//                        return $this->jsonResponse([],$_white->getErrors(),'1',ErrCode::FAILURE);
+//                    }
+//                }
+//                if(!empty($_blacklist))
+//                {
+//                    if(!$_blacklist->delete())
+//                    {
+//                        $transaction->rollBack();
+//                        return $this->jsonResponse([],$_blacklist->getErrors(),'1',ErrCode::FAILURE);
+//                    }
+//                }
                 $transaction->commit();
                 return   $this->jsonResponse([],'操作成功','0',ErrCode::SUCCESS);
 
