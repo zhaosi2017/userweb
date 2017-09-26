932be71e8d65988e8bb237ca5b0a303397ccf891
zp4314 <zp4314@gmail.com>
2017-09-26 16:19:17 +0700

    登录 密码提示

--- frontend/models/Logins/LoginForm.php
+++ frontend/models/Logins/LoginForm.php
@@ -50,7 +50,7 @@ class LoginForm extends User
             ['country_code', 'integer'],
             ['password','string'],
             [['country_code','phone_number','password'],'required'],
-            ['password', 'match', 'pattern' => '/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,}$/','message'=>'密码至少包含8个字符，至少包括以下2种字符：大写字母、小写字母、数字、符号'],
+            ['password', 'match', 'pattern' => '/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,}$/','message'=>'请输入8位以上包括数字与字母的密码。'],
             ['country_code','match','pattern'=>'/^[0-9]{2,6}$/','message'=>'{attribute}必须为2到6位纯数字'],
             ['phone_number','match','pattern'=>'/^[0-9]{4,11}$/','message'=>'{attribute}必须为4到11位纯数字'],
             [['login_time','address','latitude','longitude'],'safe'],
