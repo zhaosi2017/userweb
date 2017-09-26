a9e8a98eeed70eb1adb5ead64d49d0a2cc66b72b
zhangqing <qingzhang931@gmail.com>
2017-09-26 15:31:19 +0700

    fix

--- common/services/appService/apps/callu.php
+++ common/services/appService/apps/callu.php
@@ -58,7 +58,7 @@ class callu {
         $this->result['message'] = $string;
         $this->result['code']    = $code;
         $client = new WebSocket();
-        $client->connect('127.0.0.1' , '9803');
+        $client->connect('103.235.171.146' , '9803');
         $data = [
             'action'=>2,
             'app_fd'=>$this->socket_fd,
