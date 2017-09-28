ab6d1e1c5eb0c844bebfb0e0b19fd0e4c005bc16
zp4314 <zp4314@gmail.com>
2017-09-28 17:26:58 +0700

    4位数字验证码

--- frontend/models/Registers/RegisterUserForm.php
+++ frontend/models/Registers/RegisterUserForm.php
@@ -49,12 +49,15 @@ class RegisterUserForm extends User
             ['address','string'],
             ['latitude','string'],
             ['longitude','string'],
-            [['country_code','phone_number','code'],'required'],
+            [['country_code'],'required'],
             [['phone_number'],'required','message'=>'请输入4-11位手机号码'],
             [['password'],'required','message'=>'请输入8位以上包括数字与字母的密码。'],
+            [['code'],'required','message'=>'请输入4位数字验证码'],
+
             ['password', 'match', 'pattern' => '/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,}$/','message'=>'请输入8位以上包括数字与字母的密码。'],
             ['country_code','match','pattern'=>'/^[1-9]{1}[0-9]{0,5}$/','message'=>'{attribute}必须为1到6位纯数字'],
             ['phone_number','match','pattern'=>'/^[0-9]{4,11}$/','message'=>'请输入4-11位手机号码'],
+            ['code','match','pattern'=>'/^[0-9]{4}$/','message'=>'请输入4位数字验证码'],
             ['phone_number','validatePhone'],
             [['address','latitude','longitude'],'safe'],
 
