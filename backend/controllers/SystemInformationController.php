e86fd9c6978a8bd33536eeb66bda3bdeb631f903
neng liu <nengliu@nengdeMacBook-Pro.local>
2017-10-03 14:39:46 +0700

    add system controller

---
+++ backend/controllers/SystemInformationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace backend\controllers;
+/**
+ * Created by PhpStorm.
+ * User: nengliu
+ * Date: 2017/10/3
+ * Time: 下午2:36
+ */
+
+class SystemInformationController extends Controller
+{
+
+}
