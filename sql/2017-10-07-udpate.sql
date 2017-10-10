15b1ad20fb95033d5e6bd736e74e191536d400fb
zp4314 <zp4314@gmail.com>
2017-10-10 15:59:19 +0700

    Update

--- sql/2017-10-07-udpate.sql
+++ sql/2017-10-07-udpate.sql
@@ -11,7 +11,7 @@ ALTER TABLE `call_record` ADD `call_id` CHAR(64) NULL DEFAULT NULL AFTER `active
 
 
 /***user表***/
-ALTER TABLE `user` ADD `account` VARCHAR(255) NULL DEFAULT NULL COMMENT '邮箱' AFTER `nickname`, ADD `token` VARCHAR(255) NULL DEFAULT NULL COMMENT '令牌' AFTER `email`, ADD `channel` VARCHAR(255) NULL DEFAULT NULL COMMENT '渠道' AFTER `token`, ADD `address` VARCHAR(255) NULL DEFAULT NULL COMMENT '注册地址' AFTER `channel`, ADD `longitude` VARCHAR(64) NULL DEFAULT NULL COMMENT '注册经度' AFTER `address`, ADD `latitude` VARCHAR(64) NULL DEFAULT NULL COMMENT '注册纬度' AFTER `longitude`, ADD `header_img` VARCHAR(255) NULL DEFAULT NULL COMMENT '头像地址' AFTER `latitude`, ADD `balance` DECIMAL(14,4) NULL DEFAULT '0.0000' COMMENT '余额' AFTER `header_img`;
+ALTER TABLE `user` ADD `account` VARCHAR(255) NULL DEFAULT NULL COMMENT '邮箱' AFTER `email`, ADD `token` VARCHAR(255) NULL DEFAULT NULL COMMENT '令牌' AFTER `email`, ADD `channel` VARCHAR(255) NULL DEFAULT NULL COMMENT '渠道' AFTER `token`, ADD `address` VARCHAR(255) NULL DEFAULT NULL COMMENT '注册地址' AFTER `channel`, ADD `longitude` VARCHAR(64) NULL DEFAULT NULL COMMENT '注册经度' AFTER `address`, ADD `latitude` VARCHAR(64) NULL DEFAULT NULL COMMENT '注册纬度' AFTER `longitude`, ADD `header_img` VARCHAR(255) NULL DEFAULT NULL COMMENT '头像地址' AFTER `latitude`, ADD `balance` DECIMAL(14,4) NULL DEFAULT '0.0000' COMMENT '余额' AFTER `header_img`;
 
 /***
 ** call_record     修改结构
@@ -33,13 +33,7 @@ ALTER TABLE `user` ADD `account` VARCHAR(255) NULL DEFAULT NULL COMMENT '邮箱'
 
 
 /***#统计好友电话**/
-CREATE TABLE `tmp_report_call` (
-`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-`type` int(11) DEFAULT NULL COMMENT '电话类型 好友 非好友',
-`number` int(11) DEFAULT NULL COMMENT '次数',
-`day` date DEFAULT NULL COMMENT '统计时间',
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 /*****渠道*****/
