c49a933cd8b713e09830b7b538fe7caa5c0c8f96
zp4314 <zp4314@gmail.com>
2017-10-14 11:10:53 +0700

    Up

--- backend/models/Reports/RetainedReportSearch.php
+++ backend/models/Reports/RetainedReportSearch.php
@@ -12,23 +12,18 @@ class RetainedReportSearch extends UserLoginLog
     public $start_date;
 
     public $data;
-    public function search($params)
+    public function search($param)
     {
 
-        $this->start_date = isset($param['RetainedReportSearch']['start_date'])  && $param['RetainedReportSearch']['start_date']?$param['RetainedReportSearch']['start_date']:date('Y-m-d');
+
+        $this->start_date = isset($param['RetainedReportSearch']['start_date'])  && $param['RetainedReportSearch']['start_date']? $param['RetainedReportSearch']['start_date']:date('Y-m-d');
 
         $startTime = strtotime($this->start_date) ;
         $endTime =  $startTime + 24*60*60;
 
 
-        $sql = User::find()->select('count(id) as id,country_code')->where(['>','reg_time',$startTime])
-            ->andWhere(['<','reg_time',$endTime])
-            ->andWhere(['not',['country_code'=>null]])
-            ->andWhere(['not',['country_code'=>'']])
-            ->indexBy('country_code')
-            ->groupBy('country_code')
-            ->createCommand()
-            ->getRawSql();
+
+
 
         $_data = User::find()->select('count(id) as id,country_code')->where(['>','reg_time',$startTime])
             ->andWhere(['<','reg_time',$endTime])
@@ -40,7 +35,7 @@ class RetainedReportSearch extends UserLoginLog
 //            ->getRawSql();
             ->all();
 
-        var_dump($sql.PHP_EOL,$_data);die;
+        echo '<pre>';var_dump($_data);die;
 
 //        $secondDay = UserLoginLog::find()->where(['>','reg_time',$startTime])->andWhere(['<','reg_time',$endTime])->select('id')->
 
