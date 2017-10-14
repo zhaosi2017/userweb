718d5d5aea6cdb5946c2b4ddfa52de4797938923
zp4314 <zp4314@gmail.com>
2017-10-14 14:24:54 +0700

    update

--- backend/models/Reports/RetainedReportSearch.php
+++ backend/models/Reports/RetainedReportSearch.php
@@ -35,9 +35,114 @@ class RetainedReportSearch extends UserLoginLog
 //            ->getRawSql();
             ->all();
 
-        echo '<pre>';var_dump($_data);die;
+        $tmp = [];
+        if(!empty($_data))
+        {
 
-//        $secondDay = UserLoginLog::find()->where(['>','reg_time',$startTime])->andWhere(['<','reg_time',$endTime])->select('id')->
+            foreach ($_data as $k => $_d)
+            {
+
+                if($_d->id)
+                {
+                    
+                $_User =User::find()->select('id')->where(['>','reg_time',$startTime])
+                    ->andWhere(['<','reg_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+
+                $_TodayLogin =  UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+                $startTime = $startTime + 86400;
+                $endTime = $endTime + 86400;
+                $_SecondLogin = UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+
+                $startTime = $startTime + 86400*3;
+                $endTime = $endTime + 86400*3;
+                $_ThirdLogin = UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+
+                $startTime = $startTime + 86400*7;
+                $endTime = $endTime + 86400*7;
+                $_SevenLogin = UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+
+                $startTime = $startTime + 86400*14;
+                $endTime = $endTime + 86400*14;
+                $_FourteenLogin = UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+
+
+                $startTime = $startTime + 86400*30;
+                $endTime = $endTime + 86400*30;
+                $_ThirtyLogin = UserLoginLog::find()->select('user_id as id')->where(['>','login_time',$startTime])
+                    ->andWhere(['<','login_time',$endTime])
+                    ->andWhere(['country_code'=>$_d['country_code']])
+                    ->distinct()
+                    ->indexBy('id')
+                    ->asArray()
+                    ->all();
+
+                $TodayIntersect =  count(array_intersect_key($_User,$_TodayLogin));
+                $SecondIntersect =  count(array_intersect_key($_User,$_SecondLogin));
+                $ThirdIntersect =  count(array_intersect_key($_User,$_ThirdLogin));
+                $SevenIntersect =  count(array_intersect_key($_User,$_SevenLogin));
+                $FourteenIntersect =  count(array_intersect_key($_User,$_FourteenLogin));
+                $ThirtyIntersect =  count(array_intersect_key($_User,$_ThirtyLogin));
+
+                $tmp[$k]['today'] = $_d->id ? $TodayIntersect/$_d->id :'0';
+                $tmp[$k]['second'] = $_d->id ? $SecondIntersect/$_d->id :'0';
+                $tmp[$k]['third'] = $_d->id ? $ThirdIntersect/$_d->id :'0';
+                $tmp[$k]['seven'] = $_d->id ? $SevenIntersect/$_d->id :'0';
+                $tmp[$k]['fourteen'] = $_d->id ? $FourteenIntersect/$_d->id :'0';
+                $tmp[$k]['thirty'] = $_d->id ? $ThirtyIntersect/$_d->id :'0';
+                }else{
+                    $tmp[$k]['today'] = '0';
+                    $tmp[$k]['second'] = '0';
+                    $tmp[$k]['third'] = '0';
+                    $tmp[$k]['seven'] = '0';
+                    $tmp[$k]['fourteen'] = '0';
+                    $tmp[$k]['thirty'] = '0';
+                }
+
+
+            }
+        }
+
+        var_dump($tmp);die;
+
 
 
 
