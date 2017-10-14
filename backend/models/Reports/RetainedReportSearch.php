c6365dccdc49e52ad912707464a365bce701e25a
zp4314 <zp4314@gmail.com>
2017-10-14 11:00:38 +0700

    UP

--- backend/models/Reports/RetainedReportSearch.php
+++ backend/models/Reports/RetainedReportSearch.php
@@ -21,7 +21,14 @@ class RetainedReportSearch extends UserLoginLog
         $endTime =  $startTime + 24*60*60;
 
 
-
+        $sql = User::find()->select('count(id) as id,country_code')->where(['>','reg_time',$startTime])
+            ->andWhere(['<','reg_time',$endTime])
+            ->andWhere(['not',['country_code'=>null]])
+            ->andWhere(['not',['country_code'=>'']])
+            ->indexBy('country_code')
+            ->groupBy('country_code')
+            ->createCommand()
+            ->getRawSql();
 
         $_data = User::find()->select('count(id) as id,country_code')->where(['>','reg_time',$startTime])
             ->andWhere(['<','reg_time',$endTime])
@@ -33,7 +40,7 @@ class RetainedReportSearch extends UserLoginLog
 //            ->getRawSql();
             ->all();
 
-        var_dump($_data);die;
+        var_dump($sql.PHP_EOL,$_data);die;
 
 //        $secondDay = UserLoginLog::find()->where(['>','reg_time',$startTime])->andWhere(['<','reg_time',$endTime])->select('id')->
 
