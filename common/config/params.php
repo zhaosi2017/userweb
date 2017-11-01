831c49f337d9dc2fc517543241d0edf1fa44ebad
zp4314 <zp4314@gmail.com>
2017-11-01 14:37:44 +0700

    Up

--- common/config/params.php
+++ common/config/params.php
@@ -8,7 +8,7 @@ return [
     'tts_third'=>'Sinch',
     'tts_loop'=>2,
     'fileBaseDomain' => 'http://103.235.171.146:1314/upload',//后台图片地址
-    'frontendBaseDomain' => 'http://103.235.171.146/',//前台图片地址
+    'frontendBaseDomain' => 'http://103.235.171.146:8080/',//前台图片地址
    //'fileBaseDomain' => 'http://103.235.171.146:1314/upload',//后台图片地址
    //'frontendBaseDomain' => 'http://www.userweb.com/',//前台图片地址
     'web_socket_reload'=>'callu@123@#!%^&',
