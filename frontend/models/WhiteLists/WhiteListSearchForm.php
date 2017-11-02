53eb72b41744ab979d1e958f5671e80c4fb48caf
zp4314 <zp4314@gmail.com>
2017-11-02 10:39:57 +0700

    Up

--- frontend/models/WhiteLists/WhiteListSearchForm.php
+++ frontend/models/WhiteLists/WhiteListSearchForm.php
@@ -37,4 +37,38 @@ class WhiteListSearchForm extends WhiteList
         return  $this->jsonResponse($data,'操作成功',0, ErrCode::SUCCESS);
 
     }
+
+
+    public function newLists($p)
+    {
+        $userId = Yii::$app->user->id ;
+        $limit =  self::PAGE_NUM;
+        $offset = $p == 0 ? 0: self::PAGE_NUM*$p;
+
+        $res = self::find()->select('white_uid')->where(['uid'=>$userId])->offset($offset)->limit($limit)->orderBy('id desc')->all();
+        $data = [];
+        if(!empty($res))
+        {
+            foreach ($res as $k => $v)
+            {
+                $_friends = Friends::find()->select('remark')->where(['user_id'=>$userId,'friend_id'=>$v['white_uid']])->one();
+                $_user = User::find()->select(['nickname','account','header_img'])->where(['id'=>$v['white_uid']])->one();
+                if(  !empty($_user)) {
+                    $data['lists'][$k]['account'] = $_user->account;
+                    $data['lists'][$k]['remark'] = isset($_friends['remark']) && $_friends['remark'] ? $_friends['remark'] : $_user['nickname'];
+                    $data['lists'][$k]['header_url'] = $_user['header_img'] ?  Yii::$app->params['frontendBaseDomain'].$_user['header_img'] :'';
+                }
+            }
+
+        }
+        if(!isset($data['lists']))
+        {
+            $data['lists'] = [];
+        }
+
+        $_tmp = User::find()->select('whitelist_switch')->where(['id'=>$userId])->one();
+        $data['whitelist_switch'] = isset($_tmp['whitelist_switch']) ? $_tmp['whitelist_switch'] : 0;
+        return  $this->jsonResponse($data,'操作成功',0, ErrCode::SUCCESS);
+
+    }
 }
